fb14e61d970abcdea10fd22998b38b4e4e3e4754
Juan <garcij07@myunitec.ac.nz>
2021-09-01 01:36:08 +1200

    Init commit

---
+++ dogmeets/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    height = models.CharField(max_length=255)
+    weight = models.CharField(max_length=255)
+    birthday = models.DateField()
+    anniversary = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + " " + self.breed
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=255)
+    creationDate = models.DateField(auto_now_add=True)
+    startTime = models.DateField()
+    users = models.ManyToManyField(User)
+    dogs = models.ManyToManyField(Dog)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
