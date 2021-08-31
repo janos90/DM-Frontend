ffcb41fd659a0f8fc5074c731fe93adff1e150e8
Juan <garcij07@myunitec.ac.nz>
2021-09-01 01:56:00 +1200

    Added views

--- dogmeets/views.py
+++ dogmeets/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from dogmeets.models import Dog, Activity
+
 
 # Create your views here.
+class DogsListView(ListView):
+    model = Dog
+    template_name = 'dogs.html'
+
+
+class DogDetailView(DetailView):
+    model = Dog
+    template_name = 'dog_detail.html'
+
+
+class AddDogView(CreateView):
+    model = Dog
+    template_name = 'add_dog.html'
+    form_class = DogForm
+
+
+class UpdateDogView(UpdateView):
+    model = Dog
+    template_name = 'update_post.html'
+    form_class = DogForm
+
+
+class DeleteDogView(DeleteView):
+    model = Dog
+    template_name = 'delete_dog.html'
+    success_url = reverse_lazy('home')
