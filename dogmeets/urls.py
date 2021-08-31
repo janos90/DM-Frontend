43cde805aa0d108aa604ab604631e578f1c1faed
Juan <garcij07@myunitec.ac.nz>
2021-09-01 01:55:55 +1200

    added URLS

---
+++ dogmeets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from dogmeets.views import DogsListView, DogDetailView, AddDogView, UpdateDogView, DeleteDogView
+
+urlpatterns = [
+    path('dogs', DogsListView.as_view(), name='dog-list')
+    path('dog/<int:pk>', DogDetailView.as_view(), name='dog-detail'),
+    path('dog/add', AddDogView.as_view(), name='add-dog'),
+    path('dog/<int:pk>/edit', UpdateDogView.as_view(), name='edit-dog'),
+    path('dog/<int:pk>/delete', DeleteDogView.as_view(), name='delete-dog'),
+]
